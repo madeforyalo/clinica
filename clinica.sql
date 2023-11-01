6685e38b46cf47f61612f818d69b8d99a2a5f566
madeforyalo <99339459+madeforyalo@users.noreply.github.com>
2023-11-01 19:31:14 -0300

    Update clinica.sql

--- clinica.sql
+++ clinica.sql
@@ -22,9 +22,6 @@ insert into usuarios values (null, "Gonzalo", "Rojas", "grojas", "pass.123", 1),
 							(null, "Matias", "Vicente", "mvicente", "pass.123", 2),
                             (null, "Sofia", "Peralta", "speralta", "pass.123", 3);
                             
-select * from usuarios;
-
-drop table pacientes;
 create table pacientes(
 pac_id int auto_increment primary key,
 pac_nom varchar(50),
@@ -36,22 +33,17 @@ pac_tel int (15),
 pac_mail varchar (50));
 
 insert into pacientes values (null, "Gonzalo", "Rojas", 35329399, '90-08-09', "M", 1540767894, "made@clinica.com");
-delete from pacientes where pac_id= 6;
-select * from pasientes;
 
 create table turnos(
 tur_id int primary key auto_increment,
 tur_doc int,
 tur_pas int,
-tur_fecha int,
-tur_hora int,
-foreign key (tur_doc) references usuarios(usu_id),
-foreign key (tur_pas) references pasientes(pas_id),
-foreign key (tur_hora) references horarios(hora_id));
-
-create table horarios(
-hora_id int primary key auto_increment,
-hora_hora time);
+tur_esp int,
+tur_fecha date,
+tur_hora time,
+foreign key (tur_esp) references especialidades(esp_id),
+foreign key (tur_doc) references doctor(doc_id),
+foreign key (tur_pas) references pacientes(pac_id));
 
 CREATE TABLE doctor (
     doc_id INT PRIMARY KEY auto_increment,
@@ -62,19 +54,5 @@ CREATE TABLE especialidades (
     esp_id INT PRIMARY KEY auto_increment,
     esp_nom VARCHAR(50)
 );
-CREATE TABLE docxesp (
-    docxesp_id int primary key auto_increment,
-    doc_id INT,
-    esp_id INT,
-    FOREIGN KEY (doc_id) REFERENCES doctor(doc_id),
-    FOREIGN KEY (esp_id) REFERENCES especialidades(esp_id)
-);
-CREATE TABLE turnos (
-    tur_id INT PRIMARY KEY,
-    doc_id INT,
-    tur_hi TIME,
-    tur_hf TIME,
-    tur_Fecha DATE,
-    FOREIGN KEY (doc_id) REFERENCES doctor(doc_id)
-);
+
 
